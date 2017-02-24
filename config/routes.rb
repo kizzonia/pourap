8ff437e2c83a2616a3d01f7018d79cc2e4d18673
fortune chibuike <chibuikefortune@hotmail.com>
2017-02-23 16:15:27 -0800

    videos

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :abouts
   resources :bloggers
 resources :videos, only: [:index, :new, :create]
+resources :homes
   root 'homes#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
