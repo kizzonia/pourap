907398833657fac168c9743e299b6b3b9316e294
fortune chibuike <chibuikefortune@hotmail.com>
2017-02-25 04:01:08 -0800

    edits

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -4,8 +4,18 @@
    unless Rails.env.development?
      Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
      Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
-    
 
+
+
+     Paperclip::Attachment.config.paperclip_defaults = {
+         storage: :s3,
+         s3_credentials: {
+           bucket: ENV.fetch('S3_BUCKET_NAME'),
+           access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+           secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+           s3_region: ENV.fetch('AWS_REGION'),
+         }
+       }
    # # config/initializers/paperclip.rb
     Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-2.amazonaws.com'
    end
