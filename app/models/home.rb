a3287a4c153aa90bcba3a4af8253a317f14631ae
fortune chibuike <chibuikefortune@hotmail.com>
2017-02-25 03:56:38 -0800

    edits

--- app/models/home.rb
+++ app/models/home.rb
@@ -1,5 +1,13 @@
 class Home < ApplicationRecord
   has_attached_file :thumbnail, styles: { large: "1200x1000#", medium: "600x500#" }
+  storage: :s3,
+  s3_credentials: {
+    bucket: ENV.fetch('S3_BUCKET_NAME'),
+    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+    s3_region: ENV.fetch('AWS_REGION'),
+  }
+}
 validates_attachment_content_type :thumbnail, content_type: /\Aimage\/.*\z/
-s3_region: ENV.fetch('AWS_REGION')
+
 end
