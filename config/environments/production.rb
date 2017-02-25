64932cce4300f6360a544b87e0e80c0329802613
fortune chibuike <chibuikefortune@hotmail.com>
2017-02-25 04:12:29 -0800

    edits

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,12 +73,17 @@ Rails.application.configure do
   config.paperclip_defaults = {
       storage: :s3,
       s3_credentials: {
-        bucket: ENV.fetch('S3_BUCKET_NAME'),
-        access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-        secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-        s3_region: ENV.fetch('AWS_REGION'),
+
       }
     }
+  config.paperclip_defaults = {
+  :storage => :s3,
+  :bucket => 'S3_BUCKET_NAME'
+  bucket: ENV.fetch('S3_BUCKET_NAME'),
+  access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+  secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+  s3_region: ENV.fetch('AWS_REGION'),
+}
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -7,15 +7,7 @@
 
 
 
-     config.paperclip_defaults = {
-         storage: :s3,
-         s3_credentials: {
-           bucket: ENV.fetch('S3_BUCKET_NAME'),
-           access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-           secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-           s3_region: ENV.fetch('AWS_REGION'),
-         }
-       }
+
    # # config/initializers/paperclip.rb
     Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-2.amazonaws.com'
    end
