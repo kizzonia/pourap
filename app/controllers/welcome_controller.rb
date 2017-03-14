3e17ae6853aaf289e5d8888cf4765afea5598fc1
fortune chibuike <chibuikefortune@hotmail.com>
2017-03-13 22:12:50 -0700

    music index and index pages done

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,9 @@
 class WelcomeController < ApplicationController
   def index
-    @videos = Video.all.order('created_at DESC')
+    @videos = Video.all.order('created_at DESC').limit(4)
     @events = Event.all.order('created_at DESC')
+    @musics = Music.all.order('created_at DESC')
     @abouts = About.all
-    @banners = Banner.all.order('created_at DESC').limit(2)
+    @banners = Banner.all.order('created_at DESC').limit(1)
   end
 end
