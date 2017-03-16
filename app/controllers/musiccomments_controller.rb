05ddcac2bc580022fa05f220ef1fef1cea717f79
fortune chibuike <chibuikefortune@hotmail.com>
2017-03-16 13:39:56 -0700

    making changes

--- app/controllers/musiccomments_controller.rb
+++ app/controllers/musiccomments_controller.rb
@@ -1,7 +1,7 @@
 class MusiccommentsController < ApplicationController
   def created
     @music = Music.find(params[:music_id])
-    @musiccomments = @music.musiccoments.create(params[:musiccomments]).permit[:name, :body])
+    @musiccomments = @music.musiccoments.create(params[:musiccomments].permit(:name, :body))
     redirect_to musics_path(@music)
   end
 end
