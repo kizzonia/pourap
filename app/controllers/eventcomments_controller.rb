46c27419b8c8aeeb6786c84336d34bf662e9c28b
fortune chibuike <chibuikefortune@hotmail.com>
2017-03-16 13:33:28 -0700

    making changes

--- app/controllers/eventcomments_controller.rb
+++ app/controllers/eventcomments_controller.rb
@@ -1,7 +1,7 @@
 class EventcommentsController < ApplicationController
   def create
     @event = Event.find(params[:event_id])
-    @eventcomments = @event.eventcomments.create(params[:eventcomments).permit[:name, :body])
+    @eventcomments = @event.eventcomments.create(params[:eventcomments]).permit[:name, :body])
 
     redirect_to events_path(@event)
   end
