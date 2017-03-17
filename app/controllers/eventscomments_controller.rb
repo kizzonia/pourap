cfc3207afaf1a752d5949c01c70656a4c994fad0
fortune chibuike <chibuikefortune@hotmail.com>
2017-03-17 09:17:49 -0700

    dfd

---
+++ app/controllers/eventscomments_controller.rb
@@ -0,0 +1,16 @@
+class EventscommentsController < ApplicationController
+  def create
+    @event = Event.find(params[:event_id])
+    @eventscomment = @event.eventscomments.create(params[:eventscomment].permit(:name, :body))
+
+    redirect_to events_path(@event)
+  end
+  def destroy
+  @event = Event.find(params[:event_id])
+  @eventscomment = @event.eventscomments.find(params[:id])
+  @eventscomment.destroy
+
+  redirect_to event_path(@event)
+
+end
+end
