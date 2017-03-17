e664c60e617f69d69e18fb00d59c87767815213a
fortune chibuike <chibuikefortune@hotmail.com>
2017-03-16 23:54:43 -0700

    dfd

--- app/controllers/eventcomments_controller.rb
+++ app/controllers/eventcomments_controller.rb
@@ -1,8 +1,16 @@
 class EventcommentsController < ApplicationController
   def create
     @event = Event.find(params[:event_id])
-    @eventcomments = @event.eventcomments.create(params[:eventcomments].permit(:name, :body))
+    @eventcomment = @event.eventcomments.create(params[:eventcomment].permit(:name, :body))
 
     redirect_to events_path(@event)
   end
+  def destroy
+  @event = Event.find(params[:event_id])
+  @eventcomment = @event.eventcomments.find(params[:id])
+  @eventcomment.destroy
+
+  redirect_to event_path(@event)
+
+end
 end
--- app/controllers/musiccomments_controller.rb
+++ app/controllers/musiccomments_controller.rb
@@ -1,7 +1,15 @@
 class MusiccommentsController < ApplicationController
-  def created
+  def create
     @music = Music.find(params[:music_id])
-    @musiccomments = @music.musiccoments.create(params[:musiccomments].permit(:name, :body))
+    @musiccomment = @music.musiccoments.create(params[:musiccomment].permit(:name, :body))
     redirect_to musics_path(@music)
   end
+
+def destroy
+  @music = Music.find(params[:music_id])
+  @musiccomment = @music.musiccomments.find(params[:id])
+  @musiccomment.destroy
+
+  redirect_to music_path(@music)
+
 end
