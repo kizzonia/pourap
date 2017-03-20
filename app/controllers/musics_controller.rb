f2584726e6dac94a642010b3f051d4be596ef9b0
fortune chibuike <chibuikefortune@hotmail.com>
2017-03-20 14:56:19 -0700

    download and like funtions done

--- app/controllers/musics_controller.rb
+++ app/controllers/musics_controller.rb
@@ -4,7 +4,7 @@ class MusicsController < ApplicationController
 
   def index
     @musics = Music.all.order("created_at DESC")
-     @musics = Music.highest_voted.all.limit(10)
+     @musics = Music.highest_voted.all.order(:cached_votes_score => :desc).limit(10)
      @videos = Video.highest_voted.all.limit(10)
 
   end
