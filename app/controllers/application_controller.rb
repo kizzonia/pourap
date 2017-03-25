d4af8db45a075aed1f68a117ec5b45bfaf4c9a13
fortune chibuike <chibuikefortune@hotmail.com>
2017-03-25 09:13:22 -0700

    new

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,6 +16,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-  devise_parameter_sanitizer.for(:sign_up) { |u| u.permit( :email, :password, :password_confirmation :thumbnail) }
+  devise_parameter_sanitizer.for(:sign_up) { |u| u.permit( :email, :password, :password_confirmation, :thumbnail) }
   end
 end
