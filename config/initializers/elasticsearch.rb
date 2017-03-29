68cd80957325de534caaa278755e3541cb215b83
fortune chibuike <chibuikefortune@hotmail.com>
2017-03-29 04:08:20 -0700

    DF

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,2 +1,1 @@
 Elasticsearch::Model.client = Elasticsearch::Client.new url: ENV['RAILS_ENV']
-Searchkick.client = Elasticsearch::Client.new(hosts: ENV['RAILS_ENV'], retry_on_failure: true, transport_options: {request: {timeout: 250} })
